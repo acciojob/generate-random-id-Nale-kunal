ac7164f3edfe9ce09d7e0e2026f586add851d1ed
Nale-kunal <nalekunal343@gmail.com>
2025-09-15 15:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
   // write your code here
+	let ans = "";
+	let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(let i=0;i<l;i++){
+		let randomIndex= Math.floor(Math.random()*characters.length);
+		ans+= characters[randomIndex];
+	}
+	return ans;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+// const l = prompt("Enter a number.");
 alert(makeid(l));
